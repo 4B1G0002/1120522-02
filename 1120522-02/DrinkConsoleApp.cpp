787e23f239ac7a1f22630e6534a0b4c956db74c2
士釗 馮 <4b1g0002@gmail.com>
2023-05-22 11:19:33 +0800

    102

--- 1120522-02/DrinkConsoleApp.cpp
+++ 1120522-02/DrinkConsoleApp.cpp
@@ -10,10 +10,6 @@ using namespace std;
 
 void AddNewDrink(vector<DrinkItem>& drinks)
 {
-    //DrinkItem drink1("紅茶", "大杯", 60);
-    //drinks.push_back(drink1);
-    //DrinkItem drink2("紅茶", "小杯", 40);
-    //drinks.push_back(drink2);
 
     drinks.push_back(DrinkItem("紅茶", "大杯", 60));
     drinks.push_back(DrinkItem("紅茶", "小杯", 40));
@@ -30,19 +26,6 @@ void DisplayDrinkMenu(vector<DrinkItem>& drinks) {
     cout << "--------------------------------------" << endl;
 
     int i = 1;
-    //for (DrinkItem item : drinks) {
-    //    cout << setw(4) << i;
-    //    item.displayItem();
-    //    i++;
-    //}
-
-    //vector<DrinkItem>::iterator v = drinks.begin();
-    //while (v != drinks.end()) {
-    //    cout << setw(4) << i;
-    //    v->displayItem();
-    //    v++;
-    //    i++;
-    //}
 
     for (int j = 0; j < drinks.size(); j++) {
         cout << setw(4) << i;
